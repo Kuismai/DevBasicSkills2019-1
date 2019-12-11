3ef16bafc5c553d2765c83259db86c1ccf088b2b
Leo <kuismai.mu@gmail.com>
2019-12-11 12:33:24 +0200

    Custom added

--- Project/ProjectJS.js
+++ Project/ProjectJS.js
@@ -8,7 +8,8 @@ window.onload = function()
 
 
 
-function openTab(tabName) {
+function openTab(tabName)
+{
     
     var i, tabcontent, tablinks;
   
@@ -33,7 +34,8 @@ function openTab(tabName) {
 
 
 
-function conversionButton() {
+function conversionButton()
+{
     var conForm = document.forms["convertForm"];
     var inputN = conForm.elements["convertSource"].value;
     var fromsys = conForm.elements["fromNum"].value;
@@ -73,7 +75,6 @@ function conversionButton() {
 
 function dothetruth()
 {
-    var basicTable = ["p", "q", "T", "T", "T", "F", "F", "T", "F", "F"];
     var basicP = ["T", "T", "F", "F"];
     var basicQ = ["T", "F", "T", "F"];
 
@@ -88,6 +89,41 @@ function dothetruth()
         basicTruth[i][0] = basicP[i];
         basicTruth[i][1] = basicQ[i];
     }
+
+    // creating the NOT table
+    var basicNot = [];
+    for (var i = 0; i < 2; i++) {
+        basicNot[i] = new Array(2);
+    }
+
+    for (var i = 0; i < basicNot.length; i++) {
+        basicNot[i][0] = basicQ[i];
+
+        if (basicNot[i][0] == "T") {
+            basicNot[i][1] = "F";
+        }
+        else if (basicNot[i][0] == "F") {
+            basicNot[i][1] = "T";
+        }
+    }
+
+    html = "<table><tr><th>p</th><th>&not;p</th></tr><tr>";
+  
+    // loop through each row
+    for (var i = 0; i< basicNot.length; i++) {
+      for (var j = 0; j < 2; j++)
+      {
+          html += "<td>" + basicNot[i][j] + "</td>"
+      }
+        html += "</tr><tr>";
+
+    }
+
+    html += "</tr></table>";
+  
+    // input
+    document.getElementById("NOTTable").innerHTML = html;
+
 
     // creating the AND table
 
@@ -121,6 +157,7 @@ function dothetruth()
     // input
     document.getElementById("ANDTable").innerHTML = html;
 
+    // the OR table
     var basicOr = basicTruth;
     for (var i = 0; i < basicTruth.length; i++)
     {
@@ -150,4 +187,78 @@ function dothetruth()
   
     // input
     document.getElementById("ORTable").innerHTML = html;
+
+    
+    // the NOR table
+    var basicNor = basicTruth;
+    for (var i = 0; i < basicTruth.length; i++)
+    {
+        if ((basicTruth[i][0] == "F")&&(basicTruth[i][1] == "F"))
+        {
+            basicNor[i][2] = "T";
+        }
+        else
+        {
+            basicNor[i][2] = "F";
+        }
+    }
+    
+    // draw the table
+    html = "<table><tr><th>p</th><th>q</th><th>&not;(p&or;q)</th></tr><tr>";
+  
+    // loop through each row
+    for (var i = 0; i< basicNor.length; i++) {
+      for (var j = 0; j < 3; j++)
+      {
+          html += "<td>" + basicNor[i][j] + "</td>"
+      }
+        html += "</tr><tr>";
+
+    }
+    html += "</tr></table>";
+  
+    // input
+    document.getElementById("NORTable").innerHTML = html;
+
+  }
+
+
+  function dice(range)
+  {
+    var range = range;
+    
+    var result = Math.ceil(Math.random() * range);
+
+
+    switch(range)
+    {
+        case 4:     document.getElementById("d4").innerHTML = result;
+        break;
+        case 6:     document.getElementById("d6").innerHTML = result;
+        break;
+        case 8:     document.getElementById("d8").innerHTML = result;
+        break;
+        case 10:     document.getElementById("d10").innerHTML = result;
+        break;
+        case 12:     document.getElementById("d12").innerHTML = result;
+        break;
+        case 20:     document.getElementById("d20").innerHTML = result;
+        break;
+        case 100:     document.getElementById("d100").innerHTML = result;
+        break;
+
+    }
+
+  }
+
+  function cleardice()
+  {
+    document.getElementById("d4").innerHTML = "";
+    document.getElementById("d6").innerHTML = "";
+    document.getElementById("d8").innerHTML = "";
+    document.getElementById("d10").innerHTML = "";
+    document.getElementById("d12").innerHTML = "";
+    document.getElementById("d20").innerHTML = "";
+    document.getElementById("d100").innerHTML = "";
+  
   }
