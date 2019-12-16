2d4eef653b0ac1eff9b56fa0a62fd2e9c33103c4
Kuismai <32294061+Kuismai@users.noreply.github.com>
2019-12-17 00:12:21 +0200

    Cleanup and polishing

--- Project/ProjectJS.js
+++ Project/ProjectJS.js
@@ -6,22 +6,22 @@ window.onload = function()
 
 }
 
-
-
 function openTab(tabName)
 {
     
     var i, tabcontent, tablinks;
-  
-    
+      
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
+
+    for (i = 0; i < tabcontent.length; i++)
+    {
       tabcontent[i].style.display = "none";
     }
   
     
     //make sure other tabs are inactive
     tablinks = document.getElementsByClassName("tablinks");
+
     for (i = 0; i < tablinks.length; i++)
     {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
@@ -55,18 +55,25 @@ function conversionButton()
     
     var sys1 = fromNum[fromsys];
     var sys2 = toNum[tosys];
-    console.log(sys1);
-    console.log(sys2);
+    
+    var validate = CONVERT(inputN, sys1, sys1);
 
-    var result = CONVERT(inputN, sys1, sys2);
+    if (validate == inputN)
+    {
+        var result = CONVERT(inputN, sys1, sys2);
 
-    document.getElementById("convertResult").innerHTML = result;
+        document.getElementById("convertResult").innerHTML = result;
+    }
+
+    else
+    {
+        alert("Not a valid input.")
+    }
 }
 
 function CONVERT(inputN, sys1, sys2)
 { 
     var numberified = parseInt(inputN, sys1);
-    console.log(numberified);
     var result = numberified.toString(sys2);
     return result;
 }
@@ -78,7 +85,8 @@ function outputTable()
     var basicTable = [];
 
     //generate values
-    for (var i = 0; i < 51; i++) {
+    for (var i = 0; i < 51; i++)
+    {
         basicTable[i] = new Array(4);
         basicTable[i][0] = i;
         basicTable[i][1] = CONVERT(i, 10, 2);
@@ -87,22 +95,21 @@ function outputTable()
     }
 
     //actual table
-
     html = "<table><tr><th>DEC</th><th>BIN</th><th>OCT</th><th>HEX</th></tr><tr>";
   
     // loop through each row
-    for (var i = 0; i< basicTable.length; i++) {
+    for (var i = 0; i< basicTable.length; i++)
+    {
       for (var j = 0; j < 4; j++)
       {
           html += "<td>" + basicTable[i][j] + "</td>"
       }
         html += "</tr><tr>";
-
     }
 
     html += "</tr></table>";
   
-    // input
+    // output
     document.getElementById("outputTable").innerHTML = html;
 
 
@@ -118,7 +125,6 @@ function clearTable()
 
 function disableP()
 {
-    console.log("boo");
     var combo = document.getElementById("CorP");
     var perm = document.getElementsByName("permutation");
 
@@ -126,19 +132,17 @@ function disableP()
     {
         perm[0].setAttribute("disabled", "true");
         perm[1].setAttribute("disabled", "true");
-        console.log("baa");
     }
 
     if (combo.value == "permYes")
     {
         perm[0].removeAttribute("disabled");
         perm[1].removeAttribute("disabled");
-        console.log("bee");
     }
 }
+
 function comboButton()
 {
-    var mainForm = document.getElementById("combinatoricsForm");
     var permForm = document.getElementById("permChecked");
     var range = document.getElementById("comboRange").value;
     var sample = document.getElementById("comboSample").value;
@@ -147,17 +151,14 @@ function comboButton()
 
     if (choice.value == "permYes")
     {
-        console.log("permutating");
-        console.log(permForm.elements.permutation.value);
         if (permForm.elements.permutation.value == "repeat")
             {
                 for (i = 0; i < sample; i ++)
                     {
                         result *= range;
-                        console.log("wtf");
                     }
-                console.log("yes i'm counting");
             }
+        
         else
         {
            var diff = range - sample;
@@ -224,7 +225,8 @@ function dothetruth()
 
     var basicTruth = [];
 
-    for (var i = 0; i < 4; i++) { 
+    for (var i = 0; i < 4; i++)
+    { 
         basicTruth[i] = new Array(3); 
     } 
 
@@ -236,17 +238,23 @@ function dothetruth()
 
     // creating the NOT table
     var basicNot = [];
-    for (var i = 0; i < 2; i++) {
+
+    for (var i = 0; i < 2; i++)
+    {
         basicNot[i] = new Array(2);
     }
 
-    for (var i = 0; i < basicNot.length; i++) {
+    for (var i = 0; i < basicNot.length; i++)
+    {
         basicNot[i][0] = basicQ[i];
 
-        if (basicNot[i][0] == "T") {
+        if (basicNot[i][0] == "T")
+        {
             basicNot[i][1] = "F";
         }
-        else if (basicNot[i][0] == "F") {
+    
+        else if (basicNot[i][0] == "F")
+        {
             basicNot[i][1] = "T";
         }
     }
@@ -254,30 +262,33 @@ function dothetruth()
     html = "<table><tr><th>p</th><th>&not;p</th></tr><tr>";
   
     // loop through each row
-    for (var i = 0; i< basicNot.length; i++) {
-      for (var j = 0; j < 2; j++)
-      {
-          html += "<td>" + basicNot[i][j] + "</td>"
-      }
+    for (var i = 0; i< basicNot.length; i++)
+    {
+        for (var j = 0; j < 2; j++)
+        {
+            html += "<td>" + basicNot[i][j] + "</td>"
+        }
+        
         html += "</tr><tr>";
-
     }
 
     html += "</tr></table>";
   
-    // input
+    // output
     document.getElementById("NOTTable").innerHTML = html;
 
 
     // creating the AND table
 
     var basicAnd = basicTruth;
+   
     for (var i = 0; i < basicTruth.length; i++)
     {
         if ((basicTruth[i][0] == "T")&&(basicTruth[i][1] == "T"))
         {
             basicAnd[i][2] = "T";
         }
+
         else
         {
             basicAnd[i][2] = "F";
@@ -288,17 +299,19 @@ function dothetruth()
     html = "<table><tr><th>p</th><th>q</th><th>p&and;q</th></tr><tr>";
   
     // loop through each row
-    for (var i = 0; i< basicAnd.length; i++) {
-      for (var j = 0; j < 3; j++)
-      {
-          html += "<td>" + basicAnd[i][j] + "</td>"
-      }
-        html += "</tr><tr>";
+    for (var i = 0; i< basicAnd.length; i++)
+    {
+        for (var j = 0; j < 3; j++)
+        {
+            html += "<td>" + basicAnd[i][j] + "</td>"
+        }
 
+        html += "</tr><tr>";
     }
+
     html += "</tr></table>";
   
-    // input
+    // output
     document.getElementById("ANDTable").innerHTML = html;
 
     // the OR table
@@ -309,6 +322,7 @@ function dothetruth()
         {
             basicOr[i][2] = "T";
         }
+
         else
         {
             basicOr[i][2] = "F";
@@ -319,28 +333,32 @@ function dothetruth()
     html = "<table><tr><th>p</th><th>q</th><th>p&or;q</th></tr><tr>";
   
     // loop through each row
-    for (var i = 0; i< basicOr.length; i++) {
-      for (var j = 0; j < 3; j++)
-      {
-          html += "<td>" + basicOr[i][j] + "</td>"
-      }
+    for (var i = 0; i< basicOr.length; i++)
+    {
+        for (var j = 0; j < 3; j++)
+        {
+            html += "<td>" + basicOr[i][j] + "</td>"
+        }
+
         html += "</tr><tr>";
 
     }
     html += "</tr></table>";
   
-    // input
+    // output
     document.getElementById("ORTable").innerHTML = html;
 
     
     // the NOR table
     var basicNor = basicTruth;
+
     for (var i = 0; i < basicTruth.length; i++)
     {
         if ((basicTruth[i][0] == "F")&&(basicTruth[i][1] == "F"))
         {
             basicNor[i][2] = "T";
         }
+    
         else
         {
             basicNor[i][2] = "F";
@@ -351,110 +369,116 @@ function dothetruth()
     html = "<table><tr><th>p</th><th>q</th><th>&not;(p&or;q)</th></tr><tr>";
   
     // loop through each row
-    for (var i = 0; i< basicNor.length; i++) {
-      for (var j = 0; j < 3; j++)
-      {
-          html += "<td>" + basicNor[i][j] + "</td>"
-      }
+    for (var i = 0; i< basicNor.length; i++)
+    {
+        for (var j = 0; j < 3; j++)
+        {
+            html += "<td>" + basicNor[i][j] + "</td>"
+        }
+
         html += "</tr><tr>";
 
     }
+
     html += "</tr></table>";
   
-    // input
+    // output
     document.getElementById("NORTable").innerHTML = html;
 
 
      // the XOR table
      var basicXor = basicTruth;
-     for (var i = 0; i < basicTruth.length; i++)
-     {
-         if (basicTruth[i][0] == basicTruth[i][1])
-         {
-             basicXor[i][2] = "F";
-         }
-         else
-         {
+
+    for (var i = 0; i < basicTruth.length; i++)
+    {
+        if (basicTruth[i][0] == basicTruth[i][1])
+        {
+            basicXor[i][2] = "F";
+        }
+
+        else
+        {
              basicXor[i][2] = "T";
-         }
-     }
+        }
+    }
      
      // draw the table
      html = "<table><tr><th>p</th><th>q</th><th>p&#8891;q</th></tr><tr>";
    
      // loop through each row
-     for (var i = 0; i < basicXor.length; i++) {
-       for (var j = 0; j < 3; j++)
-       {
-           html += "<td>" + basicXor[i][j] + "</td>"
-       }
-         html += "</tr><tr>";
- 
-     }
+    for (var i = 0; i < basicXor.length; i++)
+    {
+        for (var j = 0; j < 3; j++)
+        {
+            html += "<td>" + basicXor[i][j] + "</td>"
+        }
+
+        html += "</tr><tr>";
+    }
      html += "</tr></table>";
    
      // input
      document.getElementById("XORTable").innerHTML = html;
 
-  }
+}
 
   // Random Values functions
 
-  function randomize ()
-  {
-      var range = document.getElementById("randomRange").value;
-      var sample = document.getElementById("randomSample").value;
-      var results = [];
+function randomize ()
+{
+    var range = document.getElementById("randomRange").value;
+    var sample = document.getElementById("randomSample").value;
+    var results = [];
 
-      for (var i = 0; i < sample; i++)
-      {
+    for (var i = 0; i < sample; i++)
+    {
         results[i] = Math.ceil(Math.random() * range);
-      }
+    }
 
-      //create a range axis
+    //create a range axis
+    var xvalues = [];
 
-      var xvalues = [];
-      for (var i = 0; i < range; i++)
-      {
-          xvalues[i] = i + 1;
-      }
+    for (var i = 0; i < range; i++)
+    {
+        xvalues[i] = i + 1;
+    }
 
-      // generate values
+    // generate values
+    var yvalues = [];
 
-      var yvalues = [];
+    for (var i = 0; i < range; i++)
+    {
+        var z = 0;
 
-      for (var i = 0; i < range; i++)
+        for (var j = 0; j < results.length; j++)
         {
-            var z = 0;
-
-            for (var j = 0; j < results.length; j++)
+            if (results[j] == i)
             {
-                if (results[j] == i)
-                {
-                    z++;
-                }
+                z++;
             }
-            yvalues[i] = z;
         }
+    
+        yvalues[i] = z;
+    }
 
 
-      var data = [
+    var data = [
           {
            x:  xvalues,
            y: yvalues,
            type: 'bar'
           }
-        ];
+    ];
            
     Plotly.newPlot('randomResult', data, {staticPlot: true});
-  }
 
-  // Custom functions
+}
 
-  function dice(range)
-  {
+  // Custom function
+
+function dice(range)
+{
     var range = range;
-    
     var result = Math.ceil(Math.random() * range);
 
 
@@ -477,10 +501,10 @@ function dothetruth()
 
     }
 
-  }
+}
 
-  function cleardice()
-  {
+function cleardice()
+{
     document.getElementById("d4").innerHTML = "";
     document.getElementById("d6").innerHTML = "";
     document.getElementById("d8").innerHTML = "";
@@ -488,5 +512,4 @@ function dothetruth()
     document.getElementById("d12").innerHTML = "";
     document.getElementById("d20").innerHTML = "";
     document.getElementById("d100").innerHTML = "";
-  
-  }
+}
